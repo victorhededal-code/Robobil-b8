74e2bb066ecafb550f15e3f5e2e5c8ede98ca17d
Madssb44 <you@example.com>
2026-01-29 10:33:07 +0100

    tof sensor

--- scr/Robotbil/sensors/TOF sensor.py
+++ scr/Robotbil/sensors/TOF sensor.py
@@ -1,12 +1,33 @@
 # målinger
+import time 
+from machine import Pin 
 
-def logik():
-    pass
 
-def wall():
-    pass
 
-def sumo():
-    pass
+gy53 = Pin(15, Pin.IN) # Initialize GY-53 I2C pin
+
+
+def measure(readout = False):
+    while gy53.value(): # Wait for the GY-53 to become ready
+        print("Waiting for GY-53 to become ready...")
+        pass
+    while not gy53.value(): # Read the GY-53 data
+        print("Reading GY-53 data...")
+        pass
+    starttime = time.ticks_us()
+    while gy53.value(): # Wait for the GY-53 to finish reading
+        print("Waiting for GY-53 to finish reading...")
+        pass
+    endtime = time.ticks_us()
+    # To get distance in mm, use (endtime - starttime) / 10
+    cm = (endtime - starttime) / 10
+    if readout:
+        print("Time elapsed: ", cm, "cm")
+
+    return cm
+
+while True:
+    measure(True)
+    time.sleep_ms(250)
 
 
