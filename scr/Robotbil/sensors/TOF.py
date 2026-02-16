b635d541c7d9f6b578850921f8fa6a6406a6cb2a
victor <victorhededal@gmail.com>
2026-02-16 13:48:23 +0100

    fixed sensor so it is no longer schizophrenic

--- scr/Robotbil/sensors/TOF.py
+++ scr/Robotbil/sensors/TOF.py
@@ -6,23 +6,30 @@ from machine import Pin
 gy53 = Pin(14, Pin.IN)  # Initialize GY-53 I2C pin
 
 
-def measure(readout=False) -> float:
+def measure(readout=False,samples=3) -> float:
     """measures the distance with the GY-53 sensor and returns the measurement in CM"""
-    while gy53.value():  # Wait for the GY-53 to become ready
-        # print("Waiting for GY-53 to become ready...")
-        pass
-    while not gy53.value():  # Read the GY-53 data
-        # print("Reading GY-53 data...")
-        pass
-    starttime = time.ticks_us()
-    while gy53.value():  # Wait for the GY-53 to finish reading
-        # print("Waiting for GY-53 to finish reading...")
-        pass
-    endtime = time.ticks_us()
-    # To get distance in mm, use (endtime - starttime) / 10 | to get distance in cm, use (endtime - starttime) / 100 
-    cm = (endtime - starttime) / 100
-    if readout:
-        print("Time elapsed: ", cm, "cm")
+    values = []
+    for i in range(samples):
+        while gy53.value():  # Wait for the GY-53 to become ready
+            # print("Waiting for GY-53 to become ready...")
+            pass
+        while not gy53.value():  # Read the GY-53 data
+            # print("Reading GY-53 data...")
+            pass
+        starttime = time.ticks_us()
+        while gy53.value():  # Wait for the GY-53 to finish reading
+            # print("Waiting for GY-53 to finish reading...")
+            pass
+        endtime = time.ticks_us()
+        # To get distance in mm, use (endtime - starttime) / 10 | to get distance in cm, use (endtime - starttime) / 100
+        cm = (endtime - starttime) / 100
+        if readout:
+            print("Time elapsed: ", cm, "cm")
+        if 0 < cm < 200:
+            values.append(cm)
 
-    return cm
+    if not values:
+        return 999
+
+    return sum(values) / len(values)
 
