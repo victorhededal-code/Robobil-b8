03f1faa98963635408f60b71b0b2b92f75a13f12
Madssb44 <you@example.com>
2026-02-25 16:42:51 +0100

    started doing some pi regulation work

--- src/lib/modes/Wall.py
+++ src/lib/modes/Wall.py
@@ -1,32 +1,37 @@
 from movement import motor
 from sensors import TOF 
 
- 
-target_dist = 30   
+target_dist = 40 
 base_speed = 40000
 
-p_val = 750 
-i_val = 0.1
+p_val = 500
+i_val = 0.005
 
-i_sum = 0.0 
+i_sum = 0 
 
 
-RC_car = Car( 16, 17, 18, 19, 20, 21, l_offset= 4 )
+RC_car = motor.Car( 16, 17, 18, 19, 20, 21, l_offset= 4 )
 
 
 def pi_calc( cm ):
-    global i_sum, p_val, i_val
+    global i_sum, p_val, i_val, target_dist, base_speed 
     error = target_dist - cm  
 
-    if not 20 < error > -20:
+    if not 25 < error > -39:
         if error > 0:
-            error = 20 
+            error = 25 
         else:
-            error = -20 
+            error = -39 
 
-    p = error * p_val 
+    p = error * p_val
+
     i_sum = i_sum + i_val * error 
 
+    if not 5 < i_sum > -5:
+        if i_sum > 5:
+            i_sum = 5
+        else:
+            i_sum = -5
 
     duty = p + i_sum 
 
@@ -37,10 +42,8 @@ def pi_calc( cm ):
     return r_duty, l_duty
     
     
-def wall_main(): 
-    global RC_car
-    r_duty, l_duty = pi_calc( get_distance_wall )
-    RC_car.custome( 1, 0, 1, 0, r_duty, l_duty )
-
-
+def wall_main():
+    global RC_car 
+    r_duty, l_duty = pi_calc( TOF.get_distance_wall() )
+    motor.RC_car.move_forward( r_duty, l_duty )
 
